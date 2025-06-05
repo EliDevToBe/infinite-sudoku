2b49c03f6fdf9fb58f340ee60a164446600fa6b4
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-06-05 20:55:24 +0200

    feat (api): Basic setup

---
+++ apps/api/index.ts
@@ -0,0 +1,27 @@
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async function handler(request, reply) {
+  return { hello: "world" };
+});
+
+const shutdown = async () => {
+  await fastify.close();
+};
+
+const start = async () => {
+  try {
+    await fastify.listen({ host: "0.0.0.0", port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+start();
