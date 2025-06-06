d4ebbdcd31d2eb6f0f2e3fca5c31c2abe1cb87e5
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-06-06 03:21:35 +0200

    Consistent space instead of tab use

--- packages/shared/utils/wait.ts
+++ packages/shared/utils/wait.ts
@@ -1,11 +1,11 @@
 import { centerText } from "./center-text";
 
 export const wait = async () => {
-	console.log(centerText(" -  - --== Waiting for db 😭 ==--- -  - "));
-	await new Promise((resolve) => {
-		setTimeout(resolve, 2000);
-	});
+  console.log(centerText(" -  - --== Waiting for db 😭 ==--- -  - "));
+  await new Promise((resolve) => {
+    setTimeout(resolve, 2000);
+  });
 
-	console.log(centerText(" -  - --== ✨ Finished ✨ ==--- -  - "));
+  console.log(centerText(" -  - --== ✨ Finished ✨ ==--- -  - "));
 };
 wait();
