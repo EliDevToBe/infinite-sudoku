814598ce9306c51bac157671ebeb56d1221ba15c
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-06-06 23:21:53 +0200

    refactor(grid-factory): Adjust fake grid creation logic

--- packages/database/faker/factories/grid-factory.ts
+++ packages/database/faker/factories/grid-factory.ts
@@ -6,7 +6,7 @@ type Grid = Prisma.gridCreateInput;
 export class GridFactory {
   constructor(
     private readonly fakerClient: Faker,
-    private readonly prisma: PrismaClient,
+    private readonly prisma: PrismaClient
   ) {}
 
   async createMany(count: number) {
@@ -14,21 +14,21 @@ export class GridFactory {
       Array.from({ length: count }, () =>
         this.prisma.grid.create({
           data: this.create(),
-        }),
-      ),
+        })
+      )
     );
   }
 
   private create(): Grid {
-    const puzzle = Array.from({ length: 8 }, () =>
-      [...Array(8)].map(() => this.fakerClient.number.int({ min: 1, max: 9 })),
+    const solution = Array.from({ length: 9 }, () =>
+      [...Array(9)].map(() => this.fakerClient.number.int({ min: 1, max: 9 }))
     );
 
-    const solution = [puzzle[0].map((_el) => 0), ...puzzle.slice(1)];
+    const puzzle = [solution[0].map((_el) => 0), ...solution.slice(1)];
 
     return {
-      puzzle,
       solution,
+      puzzle,
       difficulty: this.fakerClient.number.int({ min: 40, max: 67 }),
     };
   }
