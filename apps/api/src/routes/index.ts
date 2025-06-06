2e46616ba4cfdcc28e798d738b4d1a29751a5895
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-06-06 23:22:40 +0200

    feat (api): Implement grid controller and routes for CRUD operations

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,10 +1,12 @@
 import type { FastifyInstance, FastifyPluginOptions } from "fastify";
 import fp from "fastify-plugin";
 import userRoutes from "./user.routes";
+import gridRoutes from "./grid.routes";
 
 export default fp(
   (server: FastifyInstance, _opts: FastifyPluginOptions, done) => {
     server.register(userRoutes);
+    server.register(gridRoutes);
 
     done();
   }
