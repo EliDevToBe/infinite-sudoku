ee3bf9720b6829e5e6590b4be5574abbdf5bc3bf
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-06-06 02:32:47 +0200

    feat: Enhance faker seeder to include UserGridFactory and update user_grid schema with unique constraint

--- packages/database/faker/index.ts
+++ packages/database/faker/index.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client";
 import { centerText } from "../../shared/utils/center-text";
 import { GridFactory } from "./factories/grid-factory";
 import { UserFactory } from "./factories/user-factory";
+import { UserGridFactory } from "./factories/user-grid-factory";
 
 const fakerSeeder = async () => {
   if (process.env.NODE_ENV !== "develop") {
@@ -13,10 +14,15 @@ const fakerSeeder = async () => {
   const prisma = new PrismaClient();
 
   const userFactory = new UserFactory(faker, prisma);
-  await userFactory.createMany(10);
+  const users = await userFactory.createMany(10);
+  const userIds = users.map((user) => user.id);
 
   const gridFactory = new GridFactory(faker, prisma);
-  await gridFactory.createMany(10);
+  const grids = await gridFactory.createMany(10);
+  const gridIds = grids.map((grid) => grid.id);
+
+  const userGridFactory = new UserGridFactory(faker, prisma);
+  await userGridFactory.createMany(userIds, gridIds);
 
   console.log(centerText(" -  - --== Done 👌 ==-- -  - "));
 };
