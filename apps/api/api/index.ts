7ce10bf9576f60af9449114ac958a3350d5959cc
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-06-12 21:20:32 +0200

    cleanup: remove comments

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -40,43 +40,7 @@ server.get("/", (_req, res) => {
   res.send({ status: "ok" });
 });
 
-// const shutdown = () => {
-//   server.prisma.$disconnect();
-//   server.close();
-// };
-
-// const _start = async () => {
-//   try {
-//     await server.listen({ host: "0.0.0.0", port: 3000 });
-//   } catch (err) {
-//     server.log.error(err);
-//     shutdown();
-//   }
-// };
-
-// Only valid for local development
-// _start();
-
-// Vercel serverless template
-//
 export default async (req: unknown, res: unknown) => {
   await server.ready();
   server.server.emit("request", req, res);
 };
-
-// Initialize server (but don't start listening)
-// let isReady = false;
-// const readyPromise = server.ready().then(() => {
-//   isReady = true;
-//   console.log("Fastify is ready!");
-// });
-
-// // Export the serverless handler
-// export default async (req: unknown, res: unknown) => {
-//   if (!isReady) await readyPromise;
-
-//   // Forward the request to Fastify
-//   server.server.emit("request", req, res);
-// };
-
-// export default server;
