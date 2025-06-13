e113cb1e7f278615ff3b3f3ad83eed3b76d08813
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-06-13 02:02:17 +0200

    feat(routes): implement authentication middleware

--- apps/api/src/routes/user.routes.ts
+++ apps/api/src/routes/user.routes.ts
@@ -1,18 +1,43 @@
+import type { Prisma } from "@prisma/client";
 import type { FastifyInstance, FastifyPluginOptions } from "fastify";
 import fp from "fastify-plugin";
 import { UserController } from "../controllers/user.controller";
+import { authenticated } from "../utils/auth";
+
+type UserInsert = Prisma.userCreateInput;
+type UserUpdate = Prisma.userUpdateInput;
 
 export default fp(
   (server: FastifyInstance, _opts: FastifyPluginOptions, done) => {
-    server.get("/user", UserController().getUsers);
+    server.get(
+      "/user",
+      { preHandler: authenticated },
+      UserController().getUsers,
+    );
 
-    server.get("/user/:id", UserController().getUser);
+    server.get<{ Params: { id: string } }>(
+      "/user/:id",
+      { preHandler: authenticated },
+      UserController().getUser,
+    );
 
-    server.post("/user", UserController().createUser);
+    server.post<{ Body: UserInsert }>(
+      "/user",
+      { preHandler: authenticated },
+      UserController().createUser,
+    );
 
-    server.put("/user/:id", UserController().updateUser);
+    server.put<{ Params: { id: string }; Body: UserUpdate }>(
+      "/user/:id",
+      { preHandler: authenticated },
+      UserController().updateUser,
+    );
 
-    server.delete("/user/:id", UserController().deleteUser);
+    server.delete<{ Params: { id: string } }>(
+      "/user/:id",
+      { preHandler: authenticated },
+      UserController().deleteUser,
+    );
 
     done();
   },
