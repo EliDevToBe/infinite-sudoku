f142706f59584010fef4bd9061234a68ff6a431d
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-06-13 02:25:39 +0200

    refactor(token): added type tokenPayload

--- apps/api/src/utils/token.ts
+++ apps/api/src/utils/token.ts
@@ -1,7 +1,12 @@
 import jwt from "jsonwebtoken";
 
+type TokenPayload = {
+  id: string;
+  email: string;
+};
+
 export const useToken = () => {
-  const generateToken = (payload: string) => {
+  const generateToken = (payload: TokenPayload) => {
     if (!process.env.JWT_SECRET) {
       throw new Error("JWT_SECRET is not defined");
     }
