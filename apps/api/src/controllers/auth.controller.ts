27169a189defc8b02941cd77af5031ce0617a3c6
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-06-13 02:33:55 +0200

    feat(auth): add user existence check

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -17,6 +17,13 @@ export const AuthController = () => {
       const { email, password, pseudo } = request.body;
       const prisma = request.server.prisma;
 
+      const existingUser = await prisma.user.findUnique({
+        where: { email },
+      });
+      if (existingUser) {
+        return reply.status(403).send({ message: "User already exists" });
+      }
+
       const hashedPassword = await hashPassword(password);
 
       const user = await prisma.user.create({
@@ -30,7 +37,17 @@ export const AuthController = () => {
 
       const token = generateToken(tokenizedUser);
 
-      return reply.status(201).send({ user, token });
+      const authUser = {
+        id: user.id,
+        email: user.email,
+        quality: user.quality,
+        role: user.role,
+      };
+
+      return reply.status(201).send({
+        user: authUser,
+        token,
+      });
     } catch (error) {
       reply.status(500).send({ message: "Internal server error", error });
     }
