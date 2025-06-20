54fc51aac231f0cef7383f318deb247042d4a9d5
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-06-20 02:58:41 +0200

    chore: update dependencies and add theme configuration in UnoCSS

--- apps/web/uno.config.ts
+++ apps/web/uno.config.ts
@@ -1,6 +1,23 @@
+import presetAttributify from "@unocss/preset-attributify";
 import presetWind4 from "@unocss/preset-wind4";
 import { defineConfig } from "unocss";
 
 export default defineConfig({
-  presets: [presetWind4()],
+  presets: [presetWind4(), presetAttributify()],
+  theme: {
+    colors: {
+      lTheme: {
+        surface: "#F5F6F4",
+        light: "#c0caba",
+        font: "#2f3e46",
+        accent: "#228cdb",
+      },
+      dTheme: {
+        font: "#F5F6F4",
+        light: "#cad2c5",
+        surface: "#2f3e46",
+        accent: "#228cdb",
+      },
+    },
+  },
 });
