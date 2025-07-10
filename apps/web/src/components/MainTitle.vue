056ebc43a6839fc0076e4bc4a90249c0cedd7795
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-07-10 20:47:59 +0200

    wip: Update MainTitle component layout and remove unused ToggleTheme import

--- apps/web/src/components/MainTitle.vue
+++ apps/web/src/components/MainTitle.vue
@@ -1,11 +1,8 @@
 <template>
-  <div class="flex items-center justify-center">
-    <div class="title" label="title">Sudo</div>
-    <div class="title" label="title">King</div>
-    <ToggleTheme />
+  <div class="flex items-center justify-center gap-10">
+    <div class="title text-3xl rotate--12" label="title">Sudo</div>
+    <div class="title text-3xl rotate-12" label="title">King</div>
   </div>
 </template>
 
-<script setup lang="ts">
-import ToggleTheme from "./ToggleTheme.vue";
-</script>
+<script setup lang="ts"></script>
