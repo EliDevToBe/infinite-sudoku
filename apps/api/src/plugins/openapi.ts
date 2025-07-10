0bbc19b76f16f13973d1cabdbad0a9b12cb2e16d
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-07-11 00:12:29 +0200

    feat: Implement OpenAPI plugin

---
+++ apps/api/src/plugins/openapi.ts
@@ -0,0 +1,26 @@
+import FastifySwagger from "@fastify/swagger";
+import type { FastifyInstance, FastifyPluginOptions } from "fastify";
+import fp from "fastify-plugin";
+
+export default fp(
+  (server: FastifyInstance, _opts: FastifyPluginOptions, done) => {
+    server.register(FastifySwagger, {
+      openapi: {
+        info: {
+          title: "Sudoking API",
+          version: "1.0.0",
+        },
+        components: {
+          securitySchemes: {
+            apiKey: {
+              type: "apiKey",
+              name: "apiKey",
+              in: "header",
+            },
+          },
+        },
+      },
+    });
+    done();
+  },
+);
