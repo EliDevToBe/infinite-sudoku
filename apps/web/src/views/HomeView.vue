ce2453c7e2545767a4ab73e2fe8fed9417f0372b
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-08-18 01:58:26 +0200

    refactor: Update HomeView layout and integrate API fetch functionality

--- apps/web/src/views/HomeView.vue
+++ apps/web/src/views/HomeView.vue
@@ -1,33 +1,21 @@
 <template>
   <MainWrapper>
-    <div class="flex flex-col h-full items-center justify-center">
-      <h2 class="text-3xl font-bold mb-8">Choose Difficulty</h2>
-      <div class="flex flex-col md:flex-row gap-6">
-        <div
-          v-for="difficulty in difficulties"
-          :key="difficulty"
-          class="p-8 w-64 h-32 border rounded-lg cursor-pointer flex items-center justify-center text-center hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors shadow-md"
-          @click="selectDifficulty(difficulty)"
-        >
-          <h3 class="text-2xl font-semibold">
-            {{ difficulty }}
-          </h3>
-        </div>
-      </div>
+    <div :class="ui.content">
+      <button :class="ui.button" @click="fetchApi">Fetch API test</button>
     </div>
   </MainWrapper>
 </template>
 
 <script setup lang="ts">
 import MainWrapper from "../components/MainWrapper.vue";
-import { useNavigation } from "@/composables/useNavigation";
+import { useSudoku } from "@/composables/useSudoku";
 
-const { navigateTo } = useNavigation();
+const { fetchApi } = useSudoku();
 
-const difficulties = ["easy", "medium", "hard", "expert"];
-
-const selectDifficulty = (difficulty: string) => {
-  navigateTo("puzzle-choice", { difficulty });
+const ui = {
+  content: "flex flex-col h-full items-center justify-center bg-cyan-200",
+  title: "text-3xl font-bold mb-8",
+  button: "bg-red-500 text-white p-2 rounded-md cursor-pointer",
 };
 </script>
 
