5355c21494feda39ea3ada6f266f737243cb619a
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-08-21 23:32:47 +0200

    removed unused views

--- apps/web/src/views/index.ts
+++
@@ -1,6 +0,0 @@
-import DifficultyView from "./DifficultyView.vue";
-import HomeView from "./HomeView.vue";
-import PuzzlesView from "./PuzzlesView.vue";
-import SudokuGridView from "./SudokuGridView.vue";
-
-export { DifficultyView, HomeView, PuzzlesView, SudokuGridView };
