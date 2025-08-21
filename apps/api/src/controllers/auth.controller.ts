ef8df02ea94205c071edc751ccd729b717052326
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-08-21 19:19:29 +0200

    Removed inter workspace dependencies

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -1,7 +1,7 @@
 import type { Prisma } from "@prisma/client";
-import { isProduction } from "@shared/utils/index.js";
 import type { FastifyReply, FastifyRequest } from "fastify";
 import { useHash } from "../utils/hash.js";
+import { isProduction } from "../utils/isProduction.js";
 import { useToken } from "../utils/token.js";
 
 type RegisterInput = Prisma.userCreateInput;
