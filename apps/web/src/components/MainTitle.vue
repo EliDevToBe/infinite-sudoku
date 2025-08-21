2dcfa5ae1067a58569dcd1bf0d6cbb247c5c440e
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-08-21 18:46:35 +0200

    sketch: Title ideation

--- apps/web/src/components/MainTitle.vue
+++ apps/web/src/components/MainTitle.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="flex items-center justify-center gap-10">
-    <div class="title text-3xl rotate--12" label="title">Sudo</div>
-    <div class="title text-3xl rotate-12" label="title">King</div>
+    <div class="title text-3xl rotate--12" label="title">Infinite</div>
+    <div label="logo">logo</div>
+    <div class="title text-3xl rotate-12" label="title">Sudoku</div>
   </div>
 </template>
 
