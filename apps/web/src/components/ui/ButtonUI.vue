880ed31c56fce11fc44436e05c778bc2fa4042ad
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-08-23 01:20:08 +0200

    style (UI): Refactor ButtonUI component styles for improved readability and maintainability

--- apps/web/src/components/ui/ButtonUI.vue
+++ apps/web/src/components/ui/ButtonUI.vue
@@ -35,16 +35,27 @@ const ui = {
     lg: "w-24 h-12 text-lg p-2",
   },
   light: {
-    primary:
-      "bg-lTheme-light text-lTheme-font shadow-xs shadow-lTheme-font hover:shadow-sm hover:shadow-lTheme-accent",
-    secondary:
-      "bg-lTheme-surfaceOther text-lTheme-accent outline-[1px] outline-lTheme-light  outline-lTheme-font hover:shadow-sm hover:shadow-lTheme-accent",
+    primary: `
+       bg-lTheme-light text-lTheme-font
+       shadow-xs shadow-lTheme-font 
+       hover:shadow-sm hover:shadow-lTheme-accent
+        active:bg-lTheme-surfaceOther`,
+    secondary: `
+        bg-lTheme-surfaceOther text-lTheme-accent 
+        outline-[1px] outline-lTheme-light outline-lTheme-font 
+      hover:shadow-sm hover:shadow-lTheme-accent
+      active:bg-lTheme-light`,
   },
   dark: {
-    primary:
-      "bg-dTheme-surfaceOther text-dTheme-font shadow-xs shadow-dTheme-surface hover:shadow-sm hover:shadow-dTheme-accent",
-    secondary:
-      "bg-dTheme-surfaceOther text-dTheme-font hover:shadow-sm hover:shadow-dTheme-accent",
+    primary: `
+      bg-dTheme-surfaceOther text-dTheme-font
+      shadow-xs shadow-dTheme-surface
+      hover:shadow-sm hover:shadow-dTheme-accent
+      active:bg-dTheme-surface active:text-dTheme-light`,
+    secondary: `
+      bg-dTheme-surfaceOther text-dTheme-font 
+      hover:shadow-sm hover:shadow-dTheme-accent
+      active:bg-dTheme-surface active:text-dTheme-light`,
   },
 };
 </script>
