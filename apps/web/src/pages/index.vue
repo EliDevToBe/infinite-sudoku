b0c143779807d232949525cea7f1cd8df3e77841
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-08-23 01:33:31 +0200

    feat (UI): Almost full menu UI

--- apps/web/src/pages/index.vue
+++ apps/web/src/pages/index.vue
@@ -1,61 +1,76 @@
 <template>
   <MainWrapper>
     <div :class="ui.content">
-      <div :class="ui.buttonWrapper" class="bg-red-300">
+      <div :class="ui.menuWrapper" class="bg-red-300">
         <ButtonUI size="lg" @click="navigateTo('/play/')">PLAY</ButtonUI>
 
-        <div
-          v-if="!isAuthenticated"
-          class="relative flex items-center"
-          :class="verticalAnimation.join(' ')"
-        >
-          <ButtonUI
-            size="sm"
-            class="z--1 absolute isolate"
-            :class="lateralLeftAnimation.join(' ')"
+        <!-- REGISTER / LOGIN -->
+        <div class="flex flex-col items-center isolate">
+          <div
+            v-if="!isAuthenticated"
+            class="relative flex items-center"
+            :class="verticalAnimation.join(' ')"
           >
-            Register
-          </ButtonUI>
+            <ButtonUI
+              size="sm"
+              class="z--1 absolute transition-transform transition-ease"
+              :class="lateralLeftAnimation.join(' ')"
+            >
+              Register
+            </ButtonUI>
 
-          <ButtonUI
-            class="transition-transform transition-ease"
-            :class="lateralRightAnimation.join(' ')"
-            size="md"
-            @click="toggleForm"
-          >
-            Login
-          </ButtonUI>
-        </div>
+            <ButtonUI
+              class="transition-transform transition-ease"
+              :class="lateralRightAnimation.join(' ')"
+              size="md"
+              @click="toggleForm"
+            >
+              Login
+            </ButtonUI>
+          </div>
 
-        <ButtonUI v-else @click="logout"> Logout </ButtonUI>
+          <ButtonUI v-else @click="logout"> Logout </ButtonUI>
 
-        <div class="flex flex-col items-center gap-2 bg-green-300">
-          <FormField
-            name="pseudo"
-            type="text"
-            placeholder="Pseudo"
-            size="sm"
-            label="Pseudo"
-            v-model="pseudo"
-          />
+          <!-- FORM -->
+          <div v-if="isAnimatedPosition" :class="ui.formWrapper">
+            <FormField
+              name="pseudo"
+              type="text"
+              placeholder="Pseudo"
+              size="sm"
+              label="Pseudo"
+              v-model="pseudo"
+            />
 
-          <FormField
-            name="email"
-            type="email"
-            placeholder="Email"
-            size="sm"
-            label="Email"
-            v-model="email"
-          />
+            <FormField
+              name="email"
+              type="email"
+              placeholder="Email"
+              size="sm"
+              label="Email"
+              v-model="email"
+            />
 
-          <FormField
-            name="password"
-            type="password"
-            placeholder="Password"
-            size="sm"
-            label="Password"
-            v-model="password"
-          />
+            <div>
+              <FormField
+                name="password"
+                type="password"
+                placeholder="Password"
+                size="sm"
+                label="Password"
+                v-model="password"
+              />
+              <div
+                role="link"
+                class="text-[8px] text-lTheme-font place-self-center hover:underline hover:cursor-pointer"
+                @click="
+                  console.warn('Forgotten password flow not yet implemented')
+                "
+              >
+                Forgot password?
+              </div>
+            </div>
+          </div>
         </div>
 
         <ButtonUI
@@ -79,7 +94,7 @@
 import { FormField, MainWrapper } from "@/components";
 import { useAuth, useNavigation } from "@/composables";
 import { onMounted, ref, watch } from "vue";
-import { ButtonUI, InputUI } from "@/components/ui";
+import { ButtonUI } from "@/components/ui";
 
 const { login, logout, isAuthenticated, initializeAuth } = useAuth();
 const { navigateTo } = useNavigation();
@@ -87,7 +102,9 @@ const { navigateTo } = useNavigation();
 const ui = {
   content: "flex justify-center items-center h-full",
   title: "text-3xl font-bold mb-8",
-  buttonWrapper: "flex flex-col gap-2 items-center w-75 h-75",
+  menuWrapper: "flex flex-col gap-2 items-center w-75 h-75",
+  formWrapper:
+    "flex flex-col w-45 p-2 gap-2 items-center bg-dTheme-light absolute z-1 rounded-sm",
 };
 
 const showForm = ref(false);
@@ -100,11 +117,15 @@ watch(isAnimatedPosition, () => console.log(isAnimatedPosition.value));
 
 watch(showForm, () => {
   if (showForm.value) {
-    verticalAnimation.value.push("translate-y-30");
+    verticalAnimation.value.push("translate-y-44");
 
     setTimeout(() => {
       lateralRightAnimation.value.push("translate-x-20");
       lateralLeftAnimation.value.push("translate-x--20");
+
+      setTimeout(() => {
+        isAnimatedPosition.value = true;
+      }, 1000);
     }, 500);
   } else {
     lateralRightAnimation.value.pop();
@@ -112,6 +133,10 @@ watch(showForm, () => {
 
     setTimeout(() => {
       verticalAnimation.value.pop();
+
+      setTimeout(() => {
+        isAnimatedPosition.value = false;
+      }, 500);
     }, 1000);
   }
 });
