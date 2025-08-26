fef53f65663c7af5fd75974c990a6b48ddd99cc4
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-08-26 16:10:52 +0200

    refactor (auth): Simplify authentication methods by removing try-catch blocks and improving error handling

--- apps/web/src/composables/useAuth.ts
+++ apps/web/src/composables/useAuth.ts
@@ -1,6 +1,5 @@
 import { computed, ref } from "vue";
 import { throwFrontError } from "@/utils/error";
-import { Logger } from "./useLogger";
 import { useUser } from "./useUser";
 
 const accessToken = ref<string | null>(null);
@@ -29,132 +28,108 @@ export const useAuth = () => {
     password: string;
     pseudo: string;
   }) => {
-    try {
-      const response = await fetch(
-        `${import.meta.env.VITE_API_URL}/auth/register`,
-        {
-          method: "POST",
-          body: JSON.stringify(payload),
-          headers: {
-            "Content-Type": "application/json",
-          },
-          credentials: "include",
+    const response = await fetch(
+      `${import.meta.env.VITE_API_URL}/auth/register`,
+      {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: {
+          "Content-Type": "application/json",
         },
-      );
+        credentials: "include",
+      },
+    );
 
-      const data = await response.json();
-      const message = data.clientMessage;
+    const data = await response.json();
+    const message = data.clientMessage;
 
-      if (!response.ok && message) {
-        throwFrontError("Failed to register", {
-          email: payload.email,
-          message,
-        });
-      }
-
-      const token = response.headers.get("access-token");
-
-      if (token) {
-        setAccessToken(token);
-        console.debug("✅ Registered");
-      }
-
-      setCurrentUser(data.user);
-      return true;
-    } catch (error) {
-      Logger.error(error);
-      return false;
+    if (!response.ok && message) {
+      throwFrontError("Failed to register", {
+        email: payload.email,
+        message,
+      });
     }
+
+    const token = response.headers.get("access-token");
+
+    if (token) {
+      setAccessToken(token);
+      console.debug("✅ Registered");
+    }
+
+    setCurrentUser(data.user);
+    return true;
   };
 
   const login = async (email: string, password: string) => {
-    try {
-      const response = await fetch(
-        `${import.meta.env.VITE_API_URL}/auth/login`,
-        {
-          method: "POST",
-          body: JSON.stringify({ email, password }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-          credentials: "include",
-        },
-      );
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
 
-      const data = await response.json();
-      const message = data.clientMessage;
+    const data = await response.json();
+    const message = data.clientMessage;
 
-      if (!response.ok && message) {
-        throwFrontError("Failed to authenticate", {
-          email,
-          message,
-        });
-      }
-
-      const token = response.headers.get("access-token");
-
-      if (token) {
-        setAccessToken(token);
-        console.debug("✅ Logged in");
-      }
-
-      setCurrentUser(data.user);
-      return true;
-    } catch (error) {
-      Logger.error(error);
-      return false;
+    if (!response.ok && message) {
+      throwFrontError("Failed to login", {
+        email,
+        message,
+        status: response.status,
+      });
     }
+
+    const token = response.headers.get("access-token");
+
+    if (token) {
+      setAccessToken(token);
+      console.debug("✅ Logged in");
+    }
+
+    await setCurrentUser(data.user);
+    return true;
   };
 
   const logout = async () => {
-    try {
-      await fetch(`${import.meta.env.VITE_API_URL}/auth/logout`, {
-        method: "POST",
-        credentials: "include",
-      });
+    await fetch(`${import.meta.env.VITE_API_URL}/auth/logout`, {
+      method: "POST",
+      credentials: "include",
+    });
 
-      clearAccessToken();
-      setCurrentUser(null);
-      console.debug("✅ Logged out");
+    clearAccessToken();
+    setCurrentUser(null);
+    console.debug("✅ Logged out");
 
-      return true;
-    } catch (error) {
-      throwFrontError("Failed to logout", { error });
-      return false;
-    }
+    return true;
   };
 
   const refreshToken = async () => {
-    try {
-      const response = await fetch(
-        `${import.meta.env.VITE_API_URL}/auth/refresh`,
-        {
-          method: "GET",
-          credentials: "include",
-        },
-      );
+    const response = await fetch(
+      `${import.meta.env.VITE_API_URL}/auth/refresh`,
+      {
+        method: "GET",
+        credentials: "include",
+      },
+    );
 
-      if (response.ok) {
-        const accessToken = response.headers.get("access-token");
+    if (response.ok) {
+      const accessToken = response.headers.get("access-token");
 
-        if (accessToken) {
-          const data = await response.json();
+      if (accessToken) {
+        const data = await response.json();
 
-          setAccessToken(accessToken);
-          setCurrentUser(data.user);
-          return accessToken;
-        }
+        setAccessToken(accessToken);
+        setCurrentUser(data.user);
+        return accessToken;
       }
-
-      // CASE: Refresh failed
-      clearAccessToken();
-      return null;
-    } catch (error) {
-      console.error("Failed to refresh token", error);
-      clearAccessToken();
-
-      return null;
     }
+
+    // CASE: Refresh failed
+    clearAccessToken();
+    return null;
   };
 
   const initializeAuth = async () => {
