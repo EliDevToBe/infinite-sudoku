746dcdd8833a435505cb01144a0663d9bc2873f3
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-08-26 19:32:58 +0200

    feat (Play page): Base template using OptionBar

--- apps/web/src/pages/play/index.vue
+++ apps/web/src/pages/play/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>PLAY index</div>
+  <MainWrapper>
+    <template #sub-header>
+      <OptionBar />
+    </template>
+  </MainWrapper>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { MainWrapper, OptionBar } from "@/components";
+</script>
 
 <style scoped lang=""></style>
