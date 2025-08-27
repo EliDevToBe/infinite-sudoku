63bd71fff4e6364a938ba04f20a86518f4d95f88
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-08-27 15:07:01 +0200

    feat (RadioUI): Add RadioUI component

---
+++ apps/web/src/components/ui/RadioUI.vue
@@ -0,0 +1,36 @@
+<template>
+  <div :class="radioClass" @click="model = value">
+    <input
+      type="radio"
+      :value="value"
+      :name="radioGroup"
+      appearance-none
+      v-model="model"
+      :checked="model === value"
+      :id="value"
+    />
+    <label :for="value">{{ label }}</label>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+type Props = {
+  value: string;
+  radioGroup: string;
+  label: string;
+};
+
+const props = withDefaults(defineProps<Props>(), {});
+const model = defineModel<string>();
+
+const ui = {
+  base: "flex grow box-border p-1 justify-center cursor-pointer",
+  active: "bg-dTheme-surface rounded-md",
+};
+
+const radioClass = computed(() => {
+  return [ui.base, model.value === props.value ? ui.active : ""];
+});
+</script>
