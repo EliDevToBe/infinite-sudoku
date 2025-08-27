be7b7fc09853370123b2c6a137e86dc243efded2
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-08-27 15:54:40 +0200

    feat (OptionBar): Implement difficulty selection with RadioUI components

--- apps/web/src/components/OptionBar.vue
+++ apps/web/src/components/OptionBar.vue
@@ -1,9 +1,41 @@
 <template>
-  <div>
-    <h1>Option Bar</h1>
+  <div
+    class="flex w-150 items-center justify-between bg-dTheme-surfaceOther rounded-md p-1 pl-2 pr-2"
+  >
+    <RadioUI
+      value="easy"
+      radioGroup="difficulty"
+      v-model="difficulty"
+      label="Easy"
+    />
+
+    <RadioUI
+      value="medium"
+      radioGroup="difficulty"
+      v-model="difficulty"
+      label="Medium"
+    />
+
+    <RadioUI
+      value="hard"
+      radioGroup="difficulty"
+      v-model="difficulty"
+      label="Hard"
+    />
+
+    <RadioUI
+      value="hardcore"
+      radioGroup="difficulty"
+      v-model="difficulty"
+      label="Hardcore"
+    />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const difficulty = ref<string>("medium");
+</script>
 
 <style scoped></style>
