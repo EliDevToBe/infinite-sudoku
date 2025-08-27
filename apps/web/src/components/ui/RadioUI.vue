d6222476ef702ca4763b22d2c03341492721c09b
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-08-27 15:54:29 +0200

    fix (RadioUI): Enhance hover effect and update label styling

--- apps/web/src/components/ui/RadioUI.vue
+++ apps/web/src/components/ui/RadioUI.vue
@@ -1,5 +1,8 @@
 <template>
-  <div :class="radioClass" @click="model = value">
+  <div
+    :class="[radioClass, model !== value ? 'inset' : '']"
+    @click="model = value"
+  >
     <input
       type="radio"
       :value="value"
@@ -9,7 +12,7 @@
       :checked="model === value"
       :id="value"
     />
-    <label :for="value">{{ label }}</label>
+    <label class="cursor-pointer" :for="value">{{ label }}</label>
   </div>
 </template>
 
@@ -34,3 +37,12 @@ const radioClass = computed(() => {
   return [ui.base, model.value === props.value ? ui.active : ""];
 });
 </script>
+
+<style scoped lang="css">
+@media (hover: hover) {
+  .inset:hover {
+    box-shadow: inset 0px -1px 0px var(--colors-lTheme-accent);
+    transition: all 150 ease-in-out;
+  }
+}
+</style>
