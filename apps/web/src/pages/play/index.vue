48a4b50613df7188066596391d55517303f59f98
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-08-28 14:59:07 +0200

    feat (play): Template disposition

--- apps/web/src/pages/play/index.vue
+++ apps/web/src/pages/play/index.vue
@@ -2,14 +2,18 @@
   <MainWrapper>
     <template #sub-header>
       <div class="flex items-center justify-center">
-        <OptionBar />
+        <OptionBar v-model="difficulty" />
       </div>
     </template>
+    <MainContent>GRID DISPLAY</MainContent>
   </MainWrapper>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import { MainWrapper, OptionBar } from "@/components";
+
+const difficulty = ref<string>("medium");
 </script>
 
 <style scoped lang=""></style>
