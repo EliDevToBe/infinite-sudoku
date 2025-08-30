b8ff498cd422f65b1dc5920a4058a742c4053062
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-08-30 11:43:31 +0200

    chore (playground): Fix type

--- apps/web/src/pages/design/index.vue
+++ apps/web/src/pages/design/index.vue
@@ -78,11 +78,10 @@
 </template>
 
 <script setup lang="ts">
-import { usePresetToast } from "@/composables/toast";
-import { useSudoku, useUser } from "@/composables";
-import { onMounted, reactive, ref, watch } from "vue";
+import { useSudoku, useUser, usePresetToast } from "@/composables";
+import { ref, watch } from "vue";
 import { Logger } from "@/composables/useLogger";
-import type { Cell } from "@/utils";
+import type { Cell, DifficultyOptions } from "@/utils";
 // definePage({ meta: { requiresAuth: true, roles: ["admin"] } });
 
 const ui = {
@@ -96,7 +95,7 @@ const { currentUser } = useUser();
 const { toastInfo, toastSuccess, toastError } = usePresetToast();
 const { formatPuzzle, getRandomPuzzle } = useSudoku();
 
-const difficulty = ref<string>("");
+const difficulty = ref<DifficultyOptions>("medium");
 
 const showToast = () => {
   toastInfo({ description: "This is a info toast" });
