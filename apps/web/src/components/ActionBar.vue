011266e010d01565d40f3cc4cd651d6923e67c57
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-08-31 23:31:55 +0200

    refactor (ActionBar): Update undo/redo button logic and styling

--- apps/web/src/components/ActionBar.vue
+++ apps/web/src/components/ActionBar.vue
@@ -1,18 +1,26 @@
 <template>
   <div :class="ui.wrapper">
     <TooltipUI text="Undo last move">
-      <div role="button" :class="ui.icon" @click="emit('onUndo')">
-        <VueIcon role="button" :class="ui.icon" name="lucide:undo"></VueIcon>
+      <div
+        role="button"
+        :class="undoClass"
+        @click="() => canUndo && emit('onUndo')"
+      >
+        <VueIcon role="button" :class="undoClass" name="lucide:undo"></VueIcon>
       </div>
     </TooltipUI>
 
     <TooltipUI text="Redo last move">
-      <div role="button" :class="ui.icon" @click="emit('onRedo')">
-        <VueIcon role="button" :class="ui.icon" name="lucide:redo"></VueIcon>
+      <div
+        role="button"
+        :class="redoClass"
+        @click="() => canRedo && emit('onRedo')"
+      >
+        <VueIcon role="button" :class="redoClass" name="lucide:redo"></VueIcon>
       </div>
     </TooltipUI>
 
-    <TooltipUI :text="`Erase last selected cell ${dataEraseTooltip}`">
+    <TooltipUI :text="`Erase selected cell ${dataEraseTooltip}`">
       <div role="button" :class="ui.icon" @click="handleEraser">
         <VueIcon role="button" :class="ui.icon" name="lucide:eraser"></VueIcon>
       </div>
@@ -29,8 +37,10 @@
 <script setup lang="ts">
 import { computed } from "vue";
 import { useState } from "@/composables";
+import { useMoveStack } from "@/composables";
 
 const { getSelectedCell } = useState();
+const { canUndo, canRedo } = useMoveStack();
 
 const ui = {
   wrapper: [
@@ -39,7 +49,9 @@ const ui = {
     "w-[95%] w-50 sm:w-75 p-1 pl-2 pr-2",
     "bg-dTheme-surfaceOther",
   ],
-  icon: ["w-5 h-5 sm:w-7 sm:h-7 ", "sm:hover:bg-dTheme-light/10 rounded-md"],
+  icon: ["w-5 h-5 sm:w-7 sm:h-7 rounded-md"],
+  noMove: "text-gray-500",
+  hasMove: "sm:hover:bg-dTheme-light/10",
 };
 
 const emit = defineEmits<{
@@ -48,6 +60,13 @@ const emit = defineEmits<{
   onEraser: [{ x: number; y: number }];
   onNote: [];
 }>();
+
+const undoClass = computed(() => {
+  return [ui.icon, canUndo.value ? ui.hasMove : ui.noMove];
+});
+const redoClass = computed(() => {
+  return [ui.icon, canRedo.value ? ui.hasMove : ui.noMove];
+});
 
 const dataEraseTooltip = computed(() => {
   const selectedCell = getSelectedCell();
