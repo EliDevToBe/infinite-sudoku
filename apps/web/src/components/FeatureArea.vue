70c931df66c3b97ceab8533c1e898f097618a580
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-09-01 15:57:19 +0200

    feat (App, FeatureArea): Enhance responsiveness with dynamic button sizes and toast position based on window size

--- apps/web/src/components/FeatureArea.vue
+++ apps/web/src/components/FeatureArea.vue
@@ -1,9 +1,14 @@
 <template>
   <div :class="ui.wrapper">
-    <ButtonUI ref="leaderboardButton" size="md" :leadingIcon="leaderboardIcon"
+    <ButtonUI
+      ref="leaderboardButton"
+      :size="size"
+      :leadingIcon="leaderboardIcon"
       >Leaderboard</ButtonUI
     >
-    <ButtonUI size="md" leadingIcon="lucide:lock">Save progress</ButtonUI>
+    <ButtonUI ref="saveButton" :size="size" :leadingIcon="saveIcon"
+      >Save progress</ButtonUI
+    >
   </div>
 </template>
 
@@ -11,22 +16,38 @@
 import { computed, useTemplateRef } from "vue";
 import { useAuth } from "@/composables/";
 import { useElementHover } from "@vueuse/core";
+import { useWindowSize } from "@vueuse/core";
 
 const ui = {
   wrapper: ["flex items-center justify-between", "w-68 sm:w-100 p-1 px-0"],
 };
 
 const { isAuthenticated } = useAuth();
+const { width } = useWindowSize();
 
 const leaderboardButton =
   useTemplateRef<HTMLButtonElement>("leaderboardButton");
-const isHovered = useElementHover(leaderboardButton);
+const isLeaderboardBtnHovered = useElementHover(leaderboardButton);
+
+const saveButton = useTemplateRef<HTMLButtonElement>("saveButton");
+const isSaveBtnHovered = useElementHover(saveButton);
 
 const leaderboardIcon = computed(() => {
   if (!isAuthenticated.value) {
-    return isHovered.value ? "lucide:unlock" : "lucide:lock";
+    return isLeaderboardBtnHovered.value ? "lucide:unlock" : "lucide:lock";
   }
   return "lucide:crown";
+});
+
+const saveIcon = computed(() => {
+  if (!isAuthenticated.value) {
+    return isSaveBtnHovered.value ? "lucide:unlock" : "lucide:lock";
+  }
+  return "lucide:save";
+});
+
+const size = computed(() => {
+  return width.value < 640 ? "sm" : "md";
 });
 </script>
 
