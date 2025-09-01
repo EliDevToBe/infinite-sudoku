1953c96a2f22e57cd5599f923c71bc49baef2eb8
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-09-01 18:20:49 +0200

    chore: Readability ui.wrapper

--- apps/web/src/components/OptionBar.vue
+++ apps/web/src/components/OptionBar.vue
@@ -41,7 +41,8 @@ const difficulty = defineModel<DifficultyOptions>();
 
 const ui = {
   wrapper: [
-    "flex items-center justify-between rounded-md transition-all duration-200 ease-in-out",
+    "flex items-center justify-between rounded-md ",
+    "transition-all duration-200 ease-in-out",
     "w-95 sm:w-150 p-1 pl-2 pr-2 mx-2",
     "bg-dTheme-surfaceOther",
   ],
