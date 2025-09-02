d93326c753469479ef2b31e6ccd1760fd0c270d5
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-09-02 19:22:57 +0200

    refactor (ActionBar): Enhance button classes and toggle note functionality

--- apps/web/src/components/ActionBar.vue
+++ apps/web/src/components/ActionBar.vue
@@ -21,21 +21,29 @@
     </LazyTooltipUI>
 
     <LazyTooltipUI :text="`Erase selected cell ${dataEraseTooltip}`">
-      <div role="button" :class="ui.icon" @click="handleEraser">
-        <VueIcon role="button" :class="ui.icon" name="lucide:eraser"></VueIcon>
+      <div role="button" :class="eraseClass" @click="handleEraser">
+        <VueIcon
+          role="button"
+          :class="eraseClass"
+          name="lucide:eraser"
+        ></VueIcon>
       </div>
     </LazyTooltipUI>
 
     <LazyTooltipUI text="Toggle Note mode">
-      <div role="button" :class="ui.icon" @click="emit('onNote')">
-        <VueIcon role="button" :class="ui.icon" name="lucide:pencil"></VueIcon>
+      <div role="button" :class="noteClass" @click="toggleNote">
+        <VueIcon
+          role="button"
+          :class="noteClass"
+          name="lucide:pencil"
+        ></VueIcon>
       </div>
     </LazyTooltipUI>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useState } from "@/composables";
 import { useMoveStack } from "@/composables";
 import { LazyTooltipUI } from "@/components";
@@ -51,6 +59,8 @@ const ui = {
     "bg-dTheme-surfaceOther",
   ],
   icon: ["w-5 h-5 sm:w-7 sm:h-7 rounded-md"],
+  active: "active:text-dTheme-accent",
+  toggle: "text-green-500",
   noMove: "text-gray-500",
   hasMove: "sm:hover:bg-dTheme-light/10",
 };
@@ -59,14 +69,22 @@ const emit = defineEmits<{
   onUndo: [];
   onRedo: [];
   onEraser: [{ x: number; y: number }];
-  onNote: [];
+  onNote: [boolean];
 }>();
 
+const isNoteMode = ref<boolean>(false);
+
 const undoClass = computed(() => {
-  return [ui.icon, canUndo.value ? ui.hasMove : ui.noMove];
+  return [ui.icon, ui.active, canUndo.value ? ui.hasMove : ui.noMove];
 });
 const redoClass = computed(() => {
-  return [ui.icon, canRedo.value ? ui.hasMove : ui.noMove];
+  return [ui.icon, ui.active, canRedo.value ? ui.hasMove : ui.noMove];
+});
+const eraseClass = computed(() => {
+  return [ui.icon, ui.active];
+});
+const noteClass = computed(() => {
+  return [ui.icon, ui.active, isNoteMode.value ? ui.toggle : ""];
 });
 
 const dataEraseTooltip = computed(() => {
@@ -79,6 +97,11 @@ const handleEraser = () => {
   const selectedCell = getSelectedCell();
   if (!selectedCell) return;
   emit("onEraser", selectedCell);
+};
+
+const toggleNote = () => {
+  // isNoteMode.value = !isNoteMode.value;
+  emit("onNote", isNoteMode.value);
 };
 </script>
 
