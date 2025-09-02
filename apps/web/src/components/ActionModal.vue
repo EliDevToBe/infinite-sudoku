f6d413b0836de4f5a6eb5296e51756929461aa27
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-09-02 22:24:52 +0200

    feat (ActionModal): Add Special prop + Button animation

--- apps/web/src/components/ActionModal.vue
+++ apps/web/src/components/ActionModal.vue
@@ -26,6 +26,11 @@
           v-if="mainActionLabel"
           size="md"
           variant="primary"
+          :class="
+            specialMainAction
+              ? 'animate-rotating-border shadow-dTheme-accent'
+              : ''
+          "
           @click="
             emit('onMainAction');
             innerShow = false;
@@ -45,6 +50,7 @@ const props = defineProps<{
   description: string;
   mainActionLabel?: string;
   secondaryActionLabel?: string;
+  specialMainAction?: boolean;
 }>();
 
 const emit = defineEmits<{
@@ -55,4 +61,59 @@ const emit = defineEmits<{
 const innerShow = defineModel<boolean>("show");
 </script>
 
-<style scoped lang=""></style>
+<style scoped lang="css">
+@property --border-angle {
+  syntax: "<angle>";
+  initial-value: 0deg;
+  inherits: false;
+}
+
+@keyframes border-angle-rotate {
+  from {
+    --border-angle: -60deg;
+  }
+  to {
+    --border-angle: 300deg;
+  }
+}
+
+.animate-rotating-border {
+  --border-angle: -60deg;
+  border: 1px solid transparent;
+  animation: border-angle-rotate 6s infinite linear;
+
+  background: linear-gradient(
+        /* var(--colors-dTheme-surfaceOther), */
+          var(--colors-dTheme-surfaceOther)
+      )
+      padding-box,
+    conic-gradient(
+        from var(--border-angle),
+        var(--colors-dTheme-accent) 10%,
+        color-mix(in srgb, var(--colors-dTheme-light) 92%, transparent)
+      )
+      border-box;
+}
+
+.animate-rotating-border:hover {
+  animation: border-angle-rotate 1.5s infinite linear;
+
+  background: linear-gradient(var(--colors-dTheme-surfaceOther)) padding-box,
+    conic-gradient(
+        from var(--border-angle),
+        var(--colors-dTheme-accent) 10%,
+        var(--colors-dTheme-light)
+      )
+      border-box;
+}
+
+.animate-rotating-border:active {
+  background: linear-gradient(var(--colors-dTheme-surface)) padding-box,
+    conic-gradient(
+        from var(--border-angle),
+        var(--colors-dTheme-accent) 10%,
+        var(--colors-dTheme-light)
+      )
+      border-box;
+}
+</style>
