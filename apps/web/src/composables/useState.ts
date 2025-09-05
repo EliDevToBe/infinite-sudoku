d4afa1261237dc44b7e628050c98ab515de9d8aa
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-09-06 00:00:47 +0200

    feat (useState, useTheme): Enhance state management and theme storage functionality

--- apps/web/src/composables/useState.ts
+++ apps/web/src/composables/useState.ts
@@ -1,7 +1,11 @@
+import { useStorage } from "@vueuse/core";
 import { ref } from "vue";
-import type { Cell } from "@/utils";
+import type { Cell, DifficultyOptions } from "@/utils";
+
+type SudokuSave = Partial<Record<DifficultyOptions, Cell[][]>>;
 
 const selectedCell = ref<Cell | null>(null);
+const sudokuSave = useStorage<SudokuSave>("infinite-sudoku-save", {});
 
 export const useState = () => {
   const setSelectedCell = (cell: Cell | null) => {
@@ -12,5 +16,25 @@ export const useState = () => {
     return selectedCell.value;
   };
 
-  return { getSelectedCell, setSelectedCell };
+  const setSudokuSave = (difficulty: DifficultyOptions, save: Cell[][]) => {
+    if (sudokuSave.value) {
+      sudokuSave.value[difficulty] = save;
+    }
+  };
+
+  const getSudokuSave = (difficulty: DifficultyOptions) => {
+    return sudokuSave.value[difficulty] || null;
+  };
+
+  const hasSudokuSave = (difficulty: DifficultyOptions) => {
+    return !!sudokuSave.value[difficulty];
+  };
+
+  return {
+    getSelectedCell,
+    setSelectedCell,
+    getSudokuSave,
+    setSudokuSave,
+    hasSudokuSave,
+  };
 };
--- apps/web/src/composables/useTheme.ts
+++ apps/web/src/composables/useTheme.ts
@@ -2,7 +2,7 @@ import { createSharedComposable, useStorage } from "@vueuse/core";
 import { computed } from "vue";
 
 type Theme = "light" | "dark";
-const theme = useStorage<Theme>("theme", "dark");
+const theme = useStorage<Theme>("infinite-sudoku-theme", "dark");
 
 export const useTheme = createSharedComposable(() => {
   const toggleTheme = () => {
