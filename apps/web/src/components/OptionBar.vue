68c5b5ac6d4496f5375af1b189c18c71b98418cd
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-09-06 02:24:12 +0200

    style (OptionBar): Adjust padding for improved layout consistency

--- apps/web/src/components/OptionBar.vue
+++ apps/web/src/components/OptionBar.vue
@@ -40,7 +40,7 @@ const ui = {
   wrapper: [
     "flex items-center justify-between rounded-md ",
     "transition-all duration-200 ease-in-out",
-    "w-95 sm:w-150 p-1 pl-2 pr-2 mx-2",
+    "w-95 sm:w-150 p-1 sm:px-2  mx-2",
     "bg-dTheme-surfaceOther",
   ],
 };
