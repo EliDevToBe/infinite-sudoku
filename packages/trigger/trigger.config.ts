f9c1fbd8522579e3a3b8443cc75c3e673c9c354b
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-09-08 03:02:27 +0200

    feat (trigger): Prisma integration

--- packages/trigger/trigger.config.ts
+++ packages/trigger/trigger.config.ts
@@ -1,3 +1,4 @@
+import { prismaExtension } from "@trigger.dev/build/extensions/prisma";
 import { defineConfig } from "@trigger.dev/sdk/v3";
 
 export default defineConfig({
@@ -7,11 +8,11 @@ export default defineConfig({
   // The max compute seconds a task is allowed to run. If the task run exceeds this duration, it will be stopped.
   // You can override this on an individual task.
   // See https://trigger.dev/docs/runs/max-duration
-  maxDuration: 3600,
+  maxDuration: 1800,
   retries: {
     enabledInDev: true,
     default: {
-      maxAttempts: 3,
+      maxAttempts: 1,
       minTimeoutInMs: 1000,
       maxTimeoutInMs: 10000,
       factor: 2,
@@ -19,4 +20,11 @@ export default defineConfig({
     },
   },
   dirs: ["./tasks"],
+  build: {
+    extensions: [
+      prismaExtension({
+        schema: "../database/prisma/schema.prisma",
+      }),
+    ],
+  },
 });
