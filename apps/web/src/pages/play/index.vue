2bb2be07424060dcb5b8943220dbf02837865d3b
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-09-13 00:07:27 +0200

    improve (sub-header): Added bit more of margin top for better mobile use

--- apps/web/src/pages/play/index.vue
+++ apps/web/src/pages/play/index.vue
@@ -1,7 +1,7 @@
 <template>
   <MainWrapper>
     <template #sub-header>
-      <div class="flex justify-center sm:h-13 h-11">
+      <div class="mt-4 flex justify-center sm:h-13 h-11">
         <OptionBar
           v-model="currentDifficulty"
           @on-select="handleDifficultySwitch"
