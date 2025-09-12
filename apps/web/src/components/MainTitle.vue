11fa3580da98e1cd3242b9a6f0a96821bd31f860
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-09-12 05:17:10 +0200

    feat (ui): Add Infinite logo component and SVG assets for branding

--- apps/web/src/components/MainTitle.vue
+++ apps/web/src/components/MainTitle.vue
@@ -2,7 +2,9 @@
   <div :class="ui.wrapper">
     <div :class="ui.titleLeft" label="title">Infinite</div>
 
-    <div :class="ui.logo" label="logo" @click="navigateTo('/')">logo</div>
+    <div :class="ui.logo" label="logo" @click="navigateTo('/')" alt="logo">
+      <LogoUI class="absolute -translate-x-[43.5%] -top-2" />
+    </div>
 
     <div :class="ui.titleRight" label="title">Sudoku</div>
   </div>
@@ -14,8 +16,8 @@ import { useNavigation } from "@/composables";
 const { navigateTo } = useNavigation();
 
 const ui = {
-  wrapper: "flex items-center justify-center gap-10 m-b-5",
-  titleLeft: "text-3xl rotate--12",
+  wrapper: "flex items-center justify-center gap-10 m-b-5 isolate",
+  titleLeft: "z-1 text-3xl rotate--12",
   titleRight: "text-3xl rotate-12",
   logo: "cursor-pointer",
 };
