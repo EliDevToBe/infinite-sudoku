29dc9609072833c24c173dfc975e5194547f5bed
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-09-16 01:51:32 +0200

    refactor (Score): Add comments to multipliers for clarity on points projection

--- apps/web/src/composables/useScore.ts
+++ apps/web/src/composables/useScore.ts
@@ -3,10 +3,10 @@ import type { Cell, DifficultyOptions } from "@shared/utils/sudoku/helper";
 const CONFIG = {
   points_per_cell: 50,
   multipliers: {
-    easy: 0.9,
-    medium: 1.2,
-    hard: 1.5,
-    hardcore: 2.0,
+    easy: 0.9, // Points projection: 1,600 - 2,025
+    medium: 1.2, // Points projection: 2,460 - 2,960
+    hard: 1.5, // Points projection: 3,500 - 3,925
+    hardcore: 2.0, // Points projection: 5,100 - 5,600
   },
   time_penalty: { value: 100, interval_in_ms: 5 * 60 * 1000 }, // 5min * 60sec * 1000ms
 };
