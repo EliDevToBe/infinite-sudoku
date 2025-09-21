79783d8b9f2ce8e42c4bc205e08e8fdf2121a8c1
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-09-21 03:37:23 +0200

    chore (UI):  Remove default padding inside modals

--- apps/web/src/components/ui/ModalUI.vue
+++ apps/web/src/components/ui/ModalUI.vue
@@ -47,7 +47,7 @@ const ui = {
   title: "text-dTheme-font sm:text-3xl text-2xl",
   header: "flex items-center gap-1.5 p-2 px-4 sm:px-6 min-h-16",
   close: "rounded-lg hover:bg-red-800",
-  body: "border-t-1 border-t-dTheme-light/80 p-4 sm:p-6",
+  body: "border-t-1 border-t-dTheme-light/80",
   footer: "border-t-1 border-t-dTheme-light/65 gap-1.5 p-2 px-4 sm:px-6 flex",
 };
 
