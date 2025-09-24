5d1b4a9fbb9fffd92db9b3ee30c0f4656a7da1cc
Maxime <158275307+EliDevToBe@users.noreply.github.com>
2025-09-24 02:38:33 +0200

    hotfix: Ensure humanly solvable sudokus (#71)

--- packages/trigger/tasks/sudoku/bulk-new-sudoku.ts
+++ packages/trigger/tasks/sudoku/bulk-new-sudoku.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { task } from "@trigger.dev/sdk/v3";
-import { DifficultyOptions } from "../../../../apps/web/src/utils";
+import { DifficultyOptions } from "../../../../packages/shared/utils/sudoku/helper";
 import { newSudokuTask } from "./new-sudoku";
 
 const prisma = new PrismaClient({
@@ -14,9 +14,16 @@ const prisma = new PrismaClient({
 export const bulkNewSudokuTask = task({
   id: "bulk-new-sudoku",
   maxDuration: 1800, // Stop executing after 1800 secs (30 mins) of compute
-  run: async (payload: { difficulty: DifficultyOptions; count: number }) => {
+  run: async (payload: {
+    difficulty: DifficultyOptions;
+    priorityType: "pattern" | "gaussian";
+    count: number;
+  }) => {
     const batchItems = Array.from({ length: payload.count }).map(() => ({
-      payload: { difficulty: payload.difficulty },
+      payload: {
+        difficulty: payload.difficulty,
+        priorityType: payload.priorityType,
+      },
     }));
 
     const results = await newSudokuTask.batchTriggerAndWait([...batchItems]);
--- packages/trigger/tasks/sudoku/new-sudoku.ts
+++ packages/trigger/tasks/sudoku/new-sudoku.ts
@@ -1,21 +1,30 @@
 import { logger, task } from "@trigger.dev/sdk/v3";
-import { DifficultyOptions } from "../../../../apps/web/src/utils";
 import { SudokuV2 } from "../../../../packages/shared/utils/sudoku/generator-v2";
 import {
+  DifficultyOptions,
   getRangeFromDifficulty,
   prepareForDatabase,
 } from "../../../../packages/shared/utils/sudoku/helper";
-import { patternPriority } from "../../../../packages/shared/utils/sudoku/priority-algorithm";
+import {
+  gaussianPriority,
+  patternPriority,
+} from "../../../../packages/shared/utils/sudoku/priority-algorithm";
 
 export const newSudokuTask = task({
   id: "new-sudoku",
   maxDuration: 900, // Stop executing after 900 secs (15 mins) of compute
-  run: async (payload: { difficulty: DifficultyOptions }) => {
+  run: async (payload: {
+    difficulty: DifficultyOptions;
+    priorityType: "pattern" | "gaussian";
+  }) => {
     try {
       const range = getRangeFromDifficulty(payload.difficulty);
       const difficulty = range[Math.floor(Math.random() * range.length)];
 
-      const generator = new SudokuV2(difficulty, patternPriority, {
+      const priorityAlgorithm =
+        payload.priorityType === "pattern" ? patternPriority : gaussianPriority;
+
+      const generator = new SudokuV2(difficulty, priorityAlgorithm, {
         logging: false,
       });
       generator.generate();
