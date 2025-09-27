36321138e21d642b282bbaf2a5a70d6e116df084
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-09-28 00:49:58 +0200

    feat (EmailService): Enhance email service

--- apps/api/src/services/email.service.ts
+++ apps/api/src/services/email.service.ts
@@ -4,7 +4,7 @@ import { IEmailService, PasswordRecoveryPayload } from "./email.interface.js";
 export class EmailService implements IEmailService {
   private readonly client: MailtrapClient;
 
-  constructor() {
+  constructor(readonly config: { canSend: boolean }) {
     if (!process.env.MAILTRAP_TOKEN) {
       throw new Error("MAILTRAP_TOKEN is not defined");
     }
@@ -32,8 +32,15 @@ export class EmailService implements IEmailService {
     };
 
     const url = new URL(process.env.FRONTEND_URL);
-    url.pathname = "/reset-password";
+    url.pathname = "/user/password-reset";
     url.searchParams.set("token", payload.token);
+
+    if (!this.config.canSend) {
+      console.warn(
+        "[EmailService] Sending email is disabled in pre-production environment",
+      );
+      return { success: true, messageIds: [] };
+    }
 
     const result = await this.client.send({
       from: sender,
