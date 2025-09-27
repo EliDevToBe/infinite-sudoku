5f98abc74fe74fbeb39abf6c70ae369bfa90600e
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-09-27 23:30:56 +0200

    feat (router): Add user password reset route and corresponding Vue component

---
+++ apps/web/src/pages/user/password-reset.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h1>Password reset page</h1>
+    <div>{{ token }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
+
+const token = route.query.token;
+
+onMounted(() => {
+  console.log(token);
+});
+</script>
+
+<style scoped lang=""></style>
