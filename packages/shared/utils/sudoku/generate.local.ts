aac17fb082f568450ae69581d187c0bf7c850470
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-09-27 15:03:19 +0200

    utils: Added local  generator tests

--- packages/shared/utils/sudoku/generate.local.ts
+++ packages/shared/utils/sudoku/generate.local.ts
@@ -1,19 +1,39 @@
 import { SudokuV2 } from "./generator-v2";
-import { patternPriority } from "./priority-algorithm";
+import { patternPriorityV2 } from "./priority-algorithm";
 
-const difficulty = 60;
+const difficulty = 40;
+const pattern = patternPriorityV2;
+
+const logDisplayBoard = (board: number[][]) => {
+  process.stdout.write("\x1b[0;0H\x1b[2J");
+  console.log("=====================");
+
+  board.forEach((row, i) => {
+    console.log(
+      row
+        .map((col, i) => {
+          const cell = col === 0 ? "•" : col;
+          if ((i + 1) % 3 === 0 && i !== 8) return `${cell} |`;
+          return cell;
+        })
+        .join(" "),
+    );
+    if ((i + 1) % 3 === 0 && i !== 8) console.log("---------------------");
+  });
+};
 
 const main = () => {
-  const generator = new SudokuV2(difficulty, patternPriority, {
+  const generator = new SudokuV2(difficulty, pattern, {
     logging: true,
-    generatorTimeoutSeconds: 1200,
+    generatorTimeoutSeconds: 1800,
+    resetThreshold: 10,
   });
   generator.generate();
 
   const { data } = generator.getPuzzleAndSolution();
 
   console.log(data);
+  logDisplayBoard(data.puzzle);
   console.log(generator.getStats());
 };
-
 main();
