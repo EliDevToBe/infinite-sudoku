e2c3f7512d8d5647fc8f4f53a9dc4ce429b7640b
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-09-29 04:21:52 +0200

    feat (ui): Introduce FormErrorText component

---
+++ apps/web/src/components/FormErrorText.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex flex-col">
+    <span
+      v-for="(error, index) in sortedErrors"
+      :key="error.field + error.type"
+      :style="{ opacity: (100 - 15 * index) / 100 }"
+      :class="ui.errorTextClass"
+      >{{ error.message }}</span
+    >
+
+    <span
+      v-if="errors.length > 2"
+      style="opacity: 45%"
+      :class="ui.errorTextClass"
+    >
+      {{ `+${errors.length - 1} more` }}</span
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { FormError } from "@/composables/useForm";
+
+const props = defineProps<{
+  sortedErrors: FormError[];
+  errors: FormError[];
+}>();
+
+const ui = {
+  errorTextClass: "text-lTheme-danger text-[9px] sm:text-[11px] text-center",
+};
+</script>
+
+<style scoped lang=""></style>
